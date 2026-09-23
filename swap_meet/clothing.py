7d7356711f09a4f9113770bf933f382e75697a2e
Rahma Mahdi <rahmamahdi.25@gmail.com>
2026-09-23 13:38:25 -0700

    feat: add item inheritance and child classes

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,10 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+
+class Clothing(Item):
+    def __init__(self, id=None, condition=0, fabric="Unknown"):
+        super().__init__(id, condition)
+        self.fabric = fabric
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
+        
