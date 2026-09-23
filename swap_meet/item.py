4df0d63fce91fa70904068f528b999aefcdfc6f1
Rahma Mahdi <rahmamahdi.25@gmail.com>
2026-09-23 14:19:43 -0700

    refactor: use tuple for condition descriptions

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,5 +1,6 @@
 import uuid
 
+CONDITION_DESCRIPTION = ( "Poor", "Fair", "Good", "Very Good", "Excellent", "Brand New" )
 
 class Item:
     def __init__(self, id=None, condition=0) -> None:
@@ -11,17 +12,7 @@ class Item:
 
     def __str__(self):
         return f"An object of type Item with id {self.id}."
-
-    def condition_description(self):
-        if self.condition == 0:
-            return "Unusable"
-        elif self.condition == 1:
-            return "Poor"
-        elif self.condition == 2:
-            return "Fair"
-        elif self.condition == 3:
-            return "Good"
-        elif self.condition == 4:
-            return "Very Good"
-        elif self.condition == 5:
-            return "Excellent"
+    
+    def condition_description(self): 
+        """Returns the description for the item's condition."""
+        return CONDITION_DESCRIPTION[self.condition]
